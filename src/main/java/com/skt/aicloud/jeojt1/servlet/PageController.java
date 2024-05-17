55316fee471d1bdd3b3485b5c481070719de36dd
goodhyoju <good_hyoju@hanmail.net>
2024-05-17 14:03:58 +0900

    Initial commit

---
+++ src/main/java/com/skt/aicloud/jeojt1/servlet/PageController.java
@@ -0,0 +1,9 @@
+package com.skt.aicloud.jeojt1.servlet;/**
+* packageName : com.skt.aicloud.jeojt1.servlet 
+* fileName : PageController 
+* author : goodhyoju 
+* date : 2024-05-17 14:00
+* description : 
+**/
+public class PageController {
+}
---
+++ src/test/java/com/skt/aicloud/jeojt1/Jeojt1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skt.aicloud.jeojt1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jeojt1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
